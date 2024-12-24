9a0864071e6e13f4a8b28106d270be1402bfaaab
Veronika Palto <nikalalin@gmail.com>
2024-12-24 16:01:38 +0300

    Adds theme switching

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+enum Themes {
+  DEFAULT = 'default',
+  LIGHT = 'light',
+  COLORFUL = 'colorful',
+};
 
 const ThemeSwitcher: React.FC = () => {
-  const onClick = (evt: React.MouseEvent<HTMLElement>) => {
+  const [currentTheme, setCurrentTheme] = useState(Themes.DEFAULT);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('selectedTheme') as Themes;
+    if (savedTheme) {
+      setCurrentTheme(savedTheme);
+      document.documentElement.className = savedTheme;
+    }
+  }, []);
+
+  useEffect(() => {
+    document.documentElement.className = currentTheme;
+    localStorage.setItem('selectedTheme', currentTheme);
+  }, [currentTheme]);
+
+  const onClick = ()=> {
+    if (currentTheme === Themes.DEFAULT) {
+      setCurrentTheme(Themes.LIGHT);
+    } else if (currentTheme === Themes.LIGHT) {
+      setCurrentTheme(Themes.COLORFUL);
+    } else if (currentTheme === Themes.COLORFUL) {
+      setCurrentTheme(Themes.DEFAULT);
+    }
   };
 
   return (
@@ -12,7 +40,7 @@ const ThemeSwitcher: React.FC = () => {
         <li>2</li>
         <li>3</li>
       </ul>
-      <button className="theme-switcher__bar" type="button" onClick={onClick}>
+      <button className="theme-switcher__bar" type="button" onClick={onClick} aria-label='Switch Theme'>
       </button>
     </div>
   )
